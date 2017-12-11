b23e6a201e185702fa7e84c263ed3a3bc291015f
Sergiy <sergey.voloshyn@gmail.com>
2017-12-11 21:02:52 +0200

    migrate to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ interface field {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'app';
